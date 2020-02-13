dacf4fc86796c8007b30c758a2c0b326fa5ebba5
YuriiMatsiv <matsiv2017@ukr.net>
2020-02-13 10:18:43 +0300

    cardBtn fix

--- src/script.js
+++ src/script.js
@@ -38,7 +38,7 @@ function showSlides() {
     }
     slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].className += " active";
-    setTimeout(showSlides, 8000); // Change image every 8 seconds
+    setTimeout(showSlides, 5000);
 }
 
 showSlides();
